a2498c9ed114df1e64c297a63e786a0d405e8abb
maizdemicorazon <maizdemicorazn@gmail.com>
2025-05-08 23:51:43 -0600

    Base app

---
+++ src/main/java/com/mdmc/posofmyheart/PosofmyheartApplication.java
@@ -0,0 +1,13 @@
+package com.mdmc.posofmyheart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosofmyheartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosofmyheartApplication.class, args);
+	}
+
+}
