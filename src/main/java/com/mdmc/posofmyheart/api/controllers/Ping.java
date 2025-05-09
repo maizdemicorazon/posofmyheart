35a0527af607308dd1b1fe80646980ff658e8070
maizdemicorazon <maizdemicorazn@gmail.com>
2025-05-09 00:57:50 -0600

    Clean files, add ping controller

---
+++ src/main/java/com/mdmc/posofmyheart/api/controllers/Ping.java
@@ -0,0 +1,25 @@
+package com.mdmc.posofmyheart.api.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * The Ping class is a REST controller that provides an endpoint for a simple health check
+ * or status response. It acts as a mechanism to verify that the service is reachable and
+ * operational by responding with a predefined string.
+ * The controller exposes the following functionality:
+ * - A GET endpoint that responds with a "pong!" message when accessed.
+ */
+@RestController
+public class Ping {
+
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("ping")
+    public String ping(){
+        return "pong!";
+    }
+
+}
