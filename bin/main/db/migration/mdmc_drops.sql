0404f638959fb23b444b366272ffe1422f34ea6b
maizdemicorazon <maizdemicorazn@gmail.com>
2025-05-09 19:36:20 -0600

    add sql files

---
+++ bin/main/db/migration/mdmc_drops.sql
@@ -0,0 +1,15 @@
+-- DROPS
+DROP VIEW current_product_prices;
+DROP VIEW sales_by_hour
+DROP VIEW sales_by_weekday
+DROP VIEW sales_by_date
+DROP VIEW top_selling_products
+DROP VIEW top_products_by_hour
+DROP VIEW top_products_by_weekday
+
+DROP TABLE order_details;
+DROP TABLE orders;
+DROP TABLE payment_methods;
+DROP TABLE product_prices;
+DROP TABLE products;
+DROP TABLE product_categories;
