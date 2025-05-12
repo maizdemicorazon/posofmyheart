04a2d19ee735ab55cd39ee75ae6f7abeba6bffa1
maizdemicorazon <maizdemicorazn@gmail.com>
2025-05-11 21:23:43 -0600

    sql data updated

--- src/main/resources/db/migration/tables_data/product_extras.sql
+++ src/main/resources/db/migration/tables_data/product_extras.sql
@@ -2,6 +2,4 @@ INSERT INTO public.product_extras ("name",description,price,"cost",active,create
 	 ('Queso Manchego Extra','Rebanada de queso extra',7.00,3.50,true,'2025-05-11 17:35:13.431034'),
 	 ('Queso Amarillo Extra','50 ml Queso amarillo liquido extra',4.00,2.00,true,'2025-05-11 17:35:13.431034'),
 	 ('Costilla extra','200g de costilla de puerco extra',20.00,10.00,true,'2025-05-11 17:35:13.431034'),
-	 ('Porción de Falda Extra','200g de falda de res deshebrada extra',22.00,11.00,true,'2025-05-11 17:35:13.431034'),
-	 ('Extra Maruchan','Sopa Maruchan adicional',15.00,7.50,true,'2025-05-11 17:35:13.431034'),
-	 ('Tostitos Extra','Bolsa de tostitos adicional',10.00,5.00,true,'2025-05-11 17:35:13.431034');
+	 ('Porción de Falda Extra','200g de falda de res deshebrada extra',22.00,11.00,true,'2025-05-11 17:35:13.431034');
