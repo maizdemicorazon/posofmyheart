223fdcb728b1fc50fc076e31b053eece833137a3
maizdemicorazon <maizdemicorazn@gmail.com>
2025-05-11 23:47:05 -0600

    add gradle wrapper

--- src/main/java/com/mdmc/posofmyheart/api/controllers/ProductController.java
+++ src/main/java/com/mdmc/posofmyheart/api/controllers/ProductController.java
@@ -1,6 +1,5 @@
 package com.mdmc.posofmyheart.api.controllers;
 
-import com.mdmc.posofmyheart.api.exceptions.ResourceNotFoundException;
 import com.mdmc.posofmyheart.application.services.ProductService;
 import com.mdmc.posofmyheart.domain.dtos.ProductsWithExtrasDto;
 import com.mdmc.posofmyheart.domain.models.Product;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/products")
@@ -21,16 +19,8 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
-    public ResponseEntity<ProductsWithExtrasDto> getMenuProducts() {
-        return Optional.ofNullable(productService.getMenuProducts())
-                .map(products -> {
-                    if (products.products() == null || products.products().isEmpty()) {
-                        throw new ResourceNotFoundException("Productos no encontrados");
-                    }
-                    return ResponseEntity.ok(products);
-                })
-                .orElseThrow(() -> new ResourceNotFoundException("Productos no encontrados"));
+    public ResponseEntity<ProductsWithExtrasDto> getMenu() {
+        return ResponseEntity.ok(productService.getMenuProducts());
     }
-
 
 }
