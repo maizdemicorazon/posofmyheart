905530af330ac2134d75503c9f98a47bdfdc0ced
maizdemicorazon <maizdemicorazn@gmail.com>
2025-05-13 16:02:49 -0600

    clean imports

--- src/main/java/com/mdmc/posofmyheart/application/dtos/OrderResponse.java
+++ src/main/java/com/mdmc/posofmyheart/application/dtos/OrderResponse.java
@@ -1,8 +1,6 @@
 package com.mdmc.posofmyheart.application.dtos;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.List;
 
 public record OrderResponse(
         Long idOrder,
--- src/main/java/com/mdmc/posofmyheart/application/mappers/OrderMapper.java
+++ src/main/java/com/mdmc/posofmyheart/application/mappers/OrderMapper.java
@@ -1,13 +1,10 @@
 package com.mdmc.posofmyheart.application.mappers;
 
 import com.mdmc.posofmyheart.application.dtos.OrderResponse;
-import com.mdmc.posofmyheart.infrastructure.persistence.entities.OrderDetailEntity;
 import com.mdmc.posofmyheart.infrastructure.persistence.entities.OrderEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
-
-import java.util.List;
 
 @Mapper
 public interface OrderMapper {
