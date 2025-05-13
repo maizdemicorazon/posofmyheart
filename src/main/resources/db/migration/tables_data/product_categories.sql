6d79ea7f773eb3c22b50880caa0d41883cae7a63
maizdemicorazon <maizdemicorazn@gmail.com>
2025-05-12 22:57:48 -0600

    remove 4th category from db

--- src/main/resources/db/migration/tables_data/product_categories.sql
+++ src/main/resources/db/migration/tables_data/product_categories.sql
@@ -1,5 +1,4 @@
 INSERT INTO public.product_categories ("name",description) VALUES
 	 ('Esquites','Variedad de esquites tradicionales y especiales'),
 	 ('Elotes','Elotes preparados de diferentes formas'),
-	 ('Extras','Ingredientes adicionales para tus esquites o elotes'),
-	 ('Bebidas','Refrescos, aguas y jugos');
+	 ('Extras','Ingredientes adicionales para tus esquites o elotes');
--- src/main/resources/db/migration/tables_data/products.sql
+++ src/main/resources/db/migration/tables_data/products.sql
@@ -8,9 +8,9 @@ INSERT INTO public.products (id_product, id_category,"name",description,image,cr
 	 (7, 1,'Esquites Tradicionales','8 onzas de esquites con mayonesa, queso y picante','https://drive.google.com/file/d/1ph3Iog_GrUF6VY2DmiapwquOCUcTDTNx/view?usp=drive_link','2025-05-11 17:35:13.431034','2025-05-11 17:35:13.431034'),
 	 (8, 2,'Elote Tradicional','Elote con mayonesa, queso y picante','https://drive.google.com/file/d/1R0z_q1JH1H7mlpSvbUt36pGbfGuUPmJk/view?usp=sharing','2025-05-11 17:35:13.431034','2025-05-11 17:35:13.431034'),
 	 (9, 2,'Elote Revolcado','Elote con topping a elegir','https://drive.google.com/file/d/1i1r2iQQIfm0TsfbCJxxlEGahcmT5TwWc/view?usp=sharing','2025-05-11 17:35:13.431034','2025-05-11 17:35:13.431034'),
-	 (10, 4,'Coca','Refresco en pet 400 ml','https://drive.google.com/file/d/1C41w2JUpvJleSgnrUiLR5s4jXek0bMUM/view?usp=sharing','2025-05-11 17:35:13.431034','2025-05-11 17:35:13.431034');
+	 (10, 3,'Coca','Refresco en pet 400 ml','https://drive.google.com/file/d/1C41w2JUpvJleSgnrUiLR5s4jXek0bMUM/view?usp=sharing','2025-05-11 17:35:13.431034','2025-05-11 17:35:13.431034');
 INSERT INTO public.products (id_product, id_category,"name",description,image,created_at,updated_at) VALUES
-	 (11, 4,'Red Cola','Refresco en pet 600 ml','https://drive.google.com/file/d/1bDM3IJmOQDtl1xz1OquqH4UFiKU90J5d/view?usp=sharing','2025-05-11 17:35:13.431034','2025-05-11 17:35:13.431034'),
-	 (12, 4,'Boing','Jugo en caja 500 ml','https://drive.google.com/file/d/1PUk_tLBQoctGdNcG1MsbrGHEE6SONwTd/view?usp=sharing','2025-05-11 17:35:13.431034','2025-05-11 17:35:13.431034'),
-	 (13, 4,'Agua del Día','Vaso de agua natural 500 ml','https://drive.google.com/file/d/1c8g7ud-wX-As_8f5Mxz2m7xv1XjFUXuv/view?usp=sharing','2025-05-11 17:35:13.431034','2025-05-11 17:35:13.431034'),
-	 (14, 4,'Jumex lata','Jugo en lata 355 ml','https://drive.google.com/file/d/1UlTILnkyeo25amhkSwTfDGAa4tteEGeg/view?usp=sharing','2025-05-11 17:35:13.431034','2025-05-11 17:35:13.431034');
+	 (11, 3,'Red Cola','Refresco en pet 600 ml','https://drive.google.com/file/d/1bDM3IJmOQDtl1xz1OquqH4UFiKU90J5d/view?usp=sharing','2025-05-11 17:35:13.431034','2025-05-11 17:35:13.431034'),
+	 (12, 3,'Boing','Jugo en caja 500 ml','https://drive.google.com/file/d/1PUk_tLBQoctGdNcG1MsbrGHEE6SONwTd/view?usp=sharing','2025-05-11 17:35:13.431034','2025-05-11 17:35:13.431034'),
+	 (13, 3,'Agua del Día','Vaso de agua natural 500 ml','https://drive.google.com/file/d/1c8g7ud-wX-As_8f5Mxz2m7xv1XjFUXuv/view?usp=sharing','2025-05-11 17:35:13.431034','2025-05-11 17:35:13.431034'),
+	 (14, 3,'Jumex lata','Jugo en lata 355 ml','https://drive.google.com/file/d/1UlTILnkyeo25amhkSwTfDGAa4tteEGeg/view?usp=sharing','2025-05-11 17:35:13.431034','2025-05-11 17:35:13.431034');
