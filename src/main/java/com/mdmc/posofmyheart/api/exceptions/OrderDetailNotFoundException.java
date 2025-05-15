ac64b47d8ad4f92afa2e9c3355b30b9802138cda
maizdemicorazon <maizdemicorazn@gmail.com>
2025-05-14 18:25:16 -0600

    Edit order (#10)
    * edit order, details and extras
    
    * edit order, details and extras

--- src/main/java/com/mdmc/posofmyheart/api/exceptions/OrderDetailNotFoundException.java
+++ src/main/java/com/mdmc/posofmyheart/api/exceptions/OrderDetailNotFoundException.java
@@ -3,6 +3,6 @@ package com.mdmc.posofmyheart.api.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="Orden no encotrada" )
-public class OrderNotFoundException extends RuntimeException {
+@ResponseStatus(value= HttpStatus.NOT_FOUND, reason="Detalle de orden no encontrado")
+public class OrderDetailNotFoundException extends RuntimeException {
 }
--- src/main/java/com/mdmc/posofmyheart/application/services/OrderService.java
+++ src/main/java/com/mdmc/posofmyheart/application/services/OrderService.java
@@ -2,6 +2,7 @@ package com.mdmc.posofmyheart.application.services;
 
 import com.mdmc.posofmyheart.application.dtos.OrderRequest;
 import com.mdmc.posofmyheart.application.dtos.OrderResponse;
+import com.mdmc.posofmyheart.application.dtos.OrderUpdateRequest;
 import com.mdmc.posofmyheart.domain.dtos.CreateOrderResponse;
 
 import java.time.LocalDate;
@@ -15,4 +16,19 @@ public interface OrderService {
     OrderResponse findOrderById(Long orderId);
 
     CreateOrderResponse createOrder(OrderRequest request);
+
+    /**
+     * <bold>Notas importantes:</bold>
+     * Para eliminar items: Simplemente no incluirlos en el array updatedItems
+     * Para eliminar extras: Incluirlos con quantity: 0 o no incluirlos en updatedExtras
+     * Para eliminar salsa: Usar "idSauce": null
+     * Campos opcionales: Todos los campos son opcionales excepto:
+     * idOrderDetail o (idProduct + idVariant) para items
+     * idExtra para extras
+     *
+     * @param idOrder - id order which belongs detail or extra
+     * @param updateRequest - body to update
+     * @return {@link OrderResponse}
+     */
+    OrderResponse updateOrder(Long idOrder, OrderUpdateRequest updateRequest);
 }
