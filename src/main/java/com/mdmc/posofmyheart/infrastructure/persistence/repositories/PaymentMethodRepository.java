540ecdf50e3592876d09e20301c0f43e8b35c024
maizdemicorazon <maizdemicorazn@gmail.com>
2025-05-20 01:50:16 -0600

    add pay methods service (#17)

--- src/main/java/com/mdmc/posofmyheart/infrastructure/persistence/repositories/PaymentMethodRepository.java
+++ src/main/java/com/mdmc/posofmyheart/infrastructure/persistence/repositories/PaymentMethodRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface PaymentMethodRepository extends JpaRepository<PaymentMethodEntity, Long> {
-    List<PaymentMethodEntity> findByActiveTrue();
 }
