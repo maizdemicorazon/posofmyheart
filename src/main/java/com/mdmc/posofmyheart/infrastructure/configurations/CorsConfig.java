23ff7ace1465de3d813e609fd6dbe4ccb2e2a8bb
maizdemicorazon <maizdemicorazn@gmail.com>
2025-06-19 02:41:51 -0600

    add cors config by class

---
+++ src/main/java/com/mdmc/posofmyheart/infrastructure/configurations/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.mdmc.posofmyheart.infrastructure.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // Para desarrollo, en producción especifica dominios
+                        //                        .allowedOrigins(
+//                                "https://posofmyheartfe-develop.up.railway.app",
+//                                "https://posofmyheartfe-develop.up.railway.app/"
+//                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(false)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
