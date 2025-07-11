0a2e6db2eaee1d20ddacced99edfaa35c147ab28
maizdemicorazon <maizdemicorazn@gmail.com>
2025-07-11 13:09:26 -0600

    change conflicting word Método by Forma

--- src/main/java/com/mdmc/posofmyheart/api/exceptions/PayMethodNotFoundException.java
+++ src/main/java/com/mdmc/posofmyheart/api/exceptions/PayMethodNotFoundException.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class PayMethodNotFoundException extends RuntimeException {
     public PayMethodNotFoundException(Long idPayment) {
-        super(String.format("Método de pago con ID %d no encontrado", idPayment));
+        super(String.format("Forma de pago con ID %d no encontrado", idPayment));
     }
 
     public PayMethodNotFoundException(String productName) {
-        super(String.format("Método de pago '%s' no encontrado", productName));
+        super(String.format("Forma de pago '%s' no encontrado", productName));
     }
 }
