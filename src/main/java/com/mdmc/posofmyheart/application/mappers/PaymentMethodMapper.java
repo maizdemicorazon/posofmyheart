f6764c2fe4d2157ab3639534208c1e8b746061f6
maizdemicorazon <maizdemicorazn@gmail.com>
2025-07-11 13:10:36 -0600

    remove useless mapper

--- src/main/java/com/mdmc/posofmyheart/application/mappers/PaymentMethodMapper.java
+++ src/main/java/com/mdmc/posofmyheart/application/mappers/PaymentMethodMapper.java
@@ -1,14 +1,9 @@
 package com.mdmc.posofmyheart.application.mappers;
 
-import com.mdmc.posofmyheart.domain.models.PaymentMethod;
-import com.mdmc.posofmyheart.infrastructure.persistence.entities.products.catalogs.PaymentMethodEntity;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface PaymentMethodMapper {
     PaymentMethodMapper INSTANCE = Mappers.getMapper(PaymentMethodMapper.class);
-
-    PaymentMethod toPaymentMethod(PaymentMethodEntity entity);
-
 }
